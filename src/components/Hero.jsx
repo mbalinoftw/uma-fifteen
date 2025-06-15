7e428a84058ab925ba69600c9d04fec704a256cf
mbalino <marianobalinho@gmail.com>
2025-06-15 02:55:37 -0300

    changed hero bg img name, cleanup files

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ScrollToSection from "./ScrollToSection";
-import bgImg from "../assets/fotos/compressed/bg-hero.jpg";
+import bgImg from "../assets/fotos/compressed/img-0.jpg";
 
 const Hero = ({ title, subtitle }) => {
     return (
         <section
             id="hero"
-            className="h-screen w-screen flex items-center justify-center bg-position-[56%_50%] md:bg-position-center bg-cover bg-fixed"
+            className="h-screen w-screen flex items-center justify-center bg-position-[56%_50%] md:bg-position-center bg-cover"
             style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${bgImg})` }}>
             <div className="flex flex-col items-center justify-center text-white">
                 <h1 className="dancing-script-700 text-6xl md:text-8xl font-bold select-none">{title}</h1>
