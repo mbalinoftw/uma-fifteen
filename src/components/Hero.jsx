d513ac82a8b5b632e5f438c2e55428fb7a4646f1
mbalino <marianobalinho@gmail.com>
2025-06-15 02:35:56 -0300

    fix hero img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,8 @@ const Hero = ({ title, subtitle }) => {
     return (
         <section
             id="hero"
-            className="h-screen w-screen flex items-center justify-center bg-position-[56%_50%] bg-cover bg-fixed"
-            style={{
-                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${bgImg})`,
-            }}>
+            className="h-screen w-screen flex items-center justify-center bg-position-[56%_50%] md:bg-position-center bg-cover bg-fixed"
+            style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${bgImg})` }}>
             <div className="flex flex-col items-center justify-center text-white">
                 <h1 className="dancing-script-700 text-6xl md:text-8xl font-bold select-none">{title}</h1>
                 <p className="text-xl md:text-2xl select-none">{subtitle}</p>
