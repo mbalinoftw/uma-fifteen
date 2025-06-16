a7b55e7210ac72e0a3589291dfd9073ccf31a8b3
mbalino <marianobalinho@gmail.com>
2025-06-16 20:14:53 -0300

    import metadata module in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import metadata from "./metadata";
 
 // https://vite.dev/config/
 export default defineConfig({
